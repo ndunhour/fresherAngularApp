b2e8238cb7864b7e02dab5d76c6e07876b678466
ndunhourowens <ndunhour@hotmail.com>
2015-12-22 21:30:25 -1000

    for loop to fill quantity dropdown

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,13 +17,27 @@ fresherControllers.controller('AdDetailCtrl', ['$scope', '$routeParams', 'Ad',
 
 fresherControllers.controller('CreateCtrl', ['$scope',
   function($scope){
+    var quantity = [];
     $scope.fish = {
       speciesSelect: null,
+      sizeSelect: null,
       species: [
         {id: '1', name: 'Ahi'},
         {id: '2', name: 'Blue Fin Tuna'},
         {id: '3', name: 'Mahi Mahi'}
-      ]
+      ],
+      size: [
+        {id: '1', size: '0-10'},
+        {id: '2', size: '10-20'},
+        {id: '3', size: '20-30'}
+      ],
+      price: [],
+      quant: quantity
     };
+    for(var i=0; i<50; i++) {
+      quantity.push({id: 'i', quantity: i});
+    }
+    $scope.fish.quantity = quantity;
+
   }
   ]);
